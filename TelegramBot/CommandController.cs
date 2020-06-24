64082f436e28f016fcbbcfb47c4b97df11fa1c89
Maxim-Issaev <m.issaev2000@gmail.com>
2020-06-24 19:06:15 +0300

    Add Commands

---
+++ TelegramBot/CommandController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Awesome
+{
+    public class CommandController
+    {
+        public CommandController(ITelegramBotClient botClient)
+        {
+            this.botClient = botClient;
+        }
+        private ITelegramBotClient botClient;
+        public List<BotCommand> Update()
+        {
+            List<BotCommand> botCommands = new List<BotCommand>();
+            BotCommand command = new BotCommand
+            {
+                Command = "time",
+                Description = "Узнать время"
+            };
+            botCommands.Add(command);
+            command = new BotCommand
+            {
+                Command = "help",
+                Description = "Помощь"
+            };
+            botCommands.Add(command);
+            return botCommands;
+
+        }
+        public async Task RunCommand(Message message)
+        {
+            string command = message.Text;
+            string outString;
+            if (command == "/help")
+            {
+                outString = "/time - узнать время";
+                await botClient.SendTextMessageAsync(text:outString,chatId: message.Chat.Id );
+            }
+            else if (command=="/time")
+            {
+                outString = Convert.ToString(DateTime.Now);
+                await botClient.SendTextMessageAsync(text: outString, chatId: message.Chat.Id);
+            }
+            else
+            {
+                outString = "Я не знаю такой команды :(";
+                await botClient.SendTextMessageAsync(text: outString, chatId: message.Chat.Id);
+            }
+        }
+    }
+}
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 
 namespace Awesome
 {
@@ -11,8 +13,10 @@ namespace Awesome
         static void Main()
         {
             botClient = new TelegramBotClient("Token");
-
             var me = botClient.GetMeAsync().Result;
+            var commandController = new CommandController(botClient);
+            List<BotCommand> botCommands = commandController.Update();
+            botClient.SetMyCommandsAsync(botCommands);
             Console.WriteLine(
               $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
             );
@@ -32,10 +36,18 @@ namespace Awesome
                 Console.WriteLine($"Text: {e.Message.Text}");
                 Logger logger = new Logger();
                 await logger.RefreshAsync(e);
-                await botClient.SendTextMessageAsync(
-                  chatId: e.Message.Chat,
-                  text: "Я получил:\n" + e.Message.Text
-                );
+                if (e.Message.Text[0] == '/')
+                {
+                    CommandController controller = new CommandController(botClient);
+                    await controller.RunCommand(e.Message);
+                }
+                else
+                {
+                    await botClient.SendTextMessageAsync(
+                      chatId: e.Message.Chat,
+                      text: "Я получил:\n" + e.Message.Text
+                    );
+                }
             }
         }
     }
